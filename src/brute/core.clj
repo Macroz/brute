6f5fffe012a934bb403ce1967f1f6a2123efda82
Markku Rontu <markku.rontu@iki.fi>
2015-11-11 18:01:00 +0200

    Refactor extent calculation

--- src/brute/core.clj
+++ src/brute/core.clj
@@ -20,16 +20,38 @@
                       attrs)]
          charts)))
 
-(defn plot1 [attrs coll]
+(defn plot1 [extent attrs coll]
+  (let [width (/ 1280 (count coll))
+        height (/ 720 (:data-height extent))
+        middle (:middle extent)]
+    (bar-chart (merge {:fill "#777"} attrs) width height middle coll)))
+
+(defn get-extent [coll]
   (let [n (count coll)
         width (/ 1280 n)
         miny (double (apply min coll))
         maxy (double (apply max coll))
         data-height (* 2.0 (Math/max (Math/abs miny) (Math/abs maxy)))
-        height (/ 720 data-height)
-        middle 360]
-    (bar-chart (merge {:fill "#777"} attrs) width height middle coll)))
+        height (/ 720 data-height)]
+    {:miny miny
+     :maxy maxy
+     :width width
+     :height height
+     :data-height data-height}))
+
+(defn combine-extents [x1 x2]
+  {:miny (Math/min (:miny x1) (:miny x2))
+   :maxy (Math/max (:maxy x1) (:maxy x2))
+   :data-height (Math/max (:data-height x1) (:data-height x2))})
+
+(defn get-extents [& data]
+  (let [extent (reduce combine-extents (map get-extent data))
+        {:keys [miny maxy data-height]} extent
+        middle (+ 360 (/ (* 720 (+ miny maxy)) data-height))]
+    (assoc extent :middle middle)))
 
 (defn plot [& plots]
   (wrap-svg {:style "margin: auto; height: 100%;"}
-            (map #(apply plot1 %) (partition 2 plots))))
+            (let [data (map second (partition 2 plots))
+                  extent (apply get-extents data)]
+              (map #(apply plot1 extent %) (partition 2 plots)))))
